31467255a148145087aa7f7f839e2fd87f694f6b
Fabian Perez Sanchez <fabian.perez92@outlook.com>
2023-06-13 01:24:33 -0300

    Subiendo proyecto

---
+++ script.js
@@ -0,0 +1,58 @@
+/* Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+Requisitos:
+- Debe funcionar solo con letras minúsculas
+- No deben ser utilizados letras con acentos ni caracteres especiales
+- Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original. */
+
+function encriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    
+    //i es para que afecte tanto mayusculas como minusculas -- e E
+    //g es para toda la linea u Oración
+    //m es para que a fecta a multiples lineas o parrafos
+    
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+
+function copiar(){
+    var codigoACopiar = document.getElementById('texto2');
+    navigator.clipboard.writeText(codigoACopiar.innerHTML);
+    document.getElementById("copiar").innerHTML = '¡COPIADO!'; 
+}
+
